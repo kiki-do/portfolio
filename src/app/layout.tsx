b3431d18cbe2617582dfe45bcaf95dc783c3c8e7
kiki-doYou <kiki1881@yandex.ru>
2023-05-17 15:52:48 +0300

    выполнен favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,25 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<Head />
-
+			<head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/public/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/public/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/public/favicon-16x16.png"
+				/>
+			</head>
 			<body className="dark:bg-stone-900">
 				<ThemeProvider enableSystem={true} attribute="class">
 					<Navbar />
